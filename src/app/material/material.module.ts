63ada624071c9409527e6d3f4a31a6629c9ed1de
Jan <juan.perezlosa@gmail.com>
2022-04-10 13:06:52 +0200

    Material Navlist

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav'; 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav'; 
 
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatIconModule,
+    MatListModule,
     MatToolbarModule,
     MatSidenavModule
   ]
