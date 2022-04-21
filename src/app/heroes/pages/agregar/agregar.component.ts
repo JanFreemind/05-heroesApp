43b3ba88b26ab53c66c2128b532aa52b5c2c14c9
JanPLosa <jplosa@tlsi.es>
2022-04-21 11:58:48 +0200

    Insertar en la base de datos

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-agregar',
@@ -29,10 +30,22 @@ export class AgregarComponent implements OnInit {
     alt_img: ''
   }
 
-  constructor() { }
+  constructor( private heroesService: HeroesService ) { }
 
   ngOnInit(): void {
     console.log('Test');
+  }
+
+  guardar() {
+    
+    if( this.heroe.superhero.trim().length === 0 ) {
+      return;
+    } 
+
+    this.heroesService.agregarHeroe( this.heroe )
+      .subscribe( resp => {
+        console.log('Respuesta', resp);
+      })
   }
 
 }
