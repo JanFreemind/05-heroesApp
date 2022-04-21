3e87ae1bb06da77fe0248509fc059132328d15d7
JanPLosa <jplosa@tlsi.es>
2022-04-21 09:43:49 +0200

    material autocomplete

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -5,7 +5,6 @@ import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
 
-
 @Injectable({
   providedIn: 'root'
 })
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -12,9 +15,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   exports: [
+    MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
     MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatIconModule,
     MatListModule,
     MatProgressSpinnerModule,
